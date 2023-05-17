3c3aac05104c2e829ec1b05a3618137b82a12880
Mohammad Shams <MohammadShams956@gmail.com>
2023-05-17 13:54:48 +0330

    headless + errors

--- Samad.py
+++ Samad.py
@@ -2,23 +2,34 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+import sys
 import time
 
-username = ""
-password = ""
+username = ''
+password = ''
 food = ''
+selfName = 'سلف مرکزی - 1'
 nextWeek = False # Should I click on the next week arrow before searching for food?
 
+refreshCount = 0
 options = Options()
 options.add_argument('--headless')
 driver = webdriver.Chrome(options=options)
 
 driver.get("https://food.guilan.ac.ir/")
+print('[INFO] Site loaded')
 
 # Login
 username_input = driver.find_element(By.NAME, 'username').send_keys(username)
 password_input = driver.find_element(By.NAME, 'password').send_keys(password)
 driver.find_element(By.NAME, "login").click()
+
+if 'نام کاربری یا رمز عبور اشتباه است' in str(driver.page_source):
+    print('[ERROR] Wrong Username or Password')
+    driver.close()
+    sys.exit(0)
+
+print('[INFO] Logged in successfully')
 
 # Select 'Reserve Food'
 driver.find_element(By.XPATH, "/html/body/div[2]/div[4]/div[1]/div/div/div[2]/div/div/div/div[1]/div/div[1]/span").click()
@@ -27,8 +38,10 @@ time.sleep(1)
 
 # Select Self
 menu = Select(driver.find_element(By.XPATH, '//*[@id="selectself_selfListId"]'))
-menu.select_by_visible_text('سلف مرکزی - 1')
+menu.select_by_visible_text(selfName)
+
 driver.find_element(By.XPATH, "//div[@id='generalAjaxDialogBodyDiv']/table/tbody/tr[3]/td/input").click()
+print('[INFO] Self Selected')
 
 if nextWeek:
     driver.find_element(By.XPATH, "/html/body/div[2]/div[4]/div[1]/div/div/div[2]/form[1]/table/tbody/tr/td/table/tbody/tr[1]/td[5]/input").click()
@@ -47,13 +60,20 @@ while True:
                 break
         if exitt:
             break
+        refreshCount += 1
+        sys.stdout.write("\rRefresh Count: {0}".format(refreshCount))
+        sys.stdout.flush()
         driver.refresh()
         
     except:
+        refreshCount += 1
+        sys.stdout.write("\rRefresh Count: {0}".format(refreshCount))
+        sys.stdout.flush()
         driver.refresh()
         pass
 
 driver.switch_to.alert.accept()
 driver.find_element(By.XPATH, "//*[@id='doReservBtn']").click()
 driver.close()
-print("Done MotherFucker B-)")
+print("[INFO] Done MotherFucker B-)")
+sys.exit(0)
